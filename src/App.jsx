88f9078ca3951e3166d6488f3638882221507006
Vynne <verena.vynne@gmail.com>
2026-05-19 16:01:12 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Player from './components/Player'
+import GameBoard from './components/GameBoard'
+import Log from './components/Log'
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([])
+  const [activePlayer, setActivePlayer] = useState('X')
+
+  function handleSelectSquare(){
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+    setGameTurns()
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className='highlight-player'>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+      <Log/>
+    </main>
+  )
+}
+
+export default App
